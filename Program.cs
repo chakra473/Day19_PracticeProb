5b83e701fc5893fa095050a624f610ed21372f6e
chakravarthy <m.chakra473@gmail.com>
2022-03-30 17:11:31 +0530

    Added program file for UC_6

--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ namespace RegexProblem
             {
                 Console.WriteLine("\nPlease Enter Password");
                 string input = Console.ReadLine();
-                Regex r = new Regex("^[0-9A-Za-z]{8,}$");
+                Regex r = new Regex("^(?=.*[A-Z])[0-9A-Za-z]{8,}$");
                 if (r.IsMatch(input))
                 {
                     Console.WriteLine("Entered Password is valid  ");
                 }
                 else
                 {
-                    Console.WriteLine("Entered Password is not valid is not valid");
+                    Console.WriteLine("Entered Password is not valid");
                 }
             }
         }
