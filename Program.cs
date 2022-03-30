ecf7fa799d25a55bed0880e542622613f6ae402b
chakravarthy <m.chakra473@gmail.com>
2022-03-30 16:53:06 +0530

    Added program file for UC_1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
-namespace Day19PracticeProblem
+namespace RegexProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to regex expression!!");
+
+            while (true)
+            {
+                Console.WriteLine("Please Enter your First name");
+                string input = Console.ReadLine();
+                Regex r = new Regex("^[A-Z][a-z]{2,}$");
+                if (r.IsMatch(input))
+                {
+                    Console.WriteLine("your First name is " + true);
+                }
+                else
+                {
+                    Console.WriteLine("Your First name is " + false);
+                    Console.WriteLine("First Name Should Contain minimum three Character and first letter should be in uppercase!!");
+                }
+            }
         }
     }
 }
